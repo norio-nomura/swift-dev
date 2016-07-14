2b70f138d2f05935f86225dda24c571cb06760d9
Norio Nomura <norio.nomura@gmail.com>
2016-07-14 23:18:42 +0900

    Update `build-Sourcekitten-in-shared-volume.sh`
    Now it builds `sourcekit:30p2` image that contains Swift 3.0 Preview 2 with `libdispatch.so` and `libsourcekitdInProc.so`

--- build-SourceKitten-in-shared-volume.sh
+++ build-SourceKitten-in-shared-volume.sh
@@ -1,38 +1,51 @@
-#!/bin/sh
-docker-machine start default && eval $(docker-machine env default)
-docker build -f Dockerfile-swift-dev-15.10 -t swift-dev-15.10 .
+#!/usr/bin/env bash
 
 cd "$(dirname $0)/." || exit
 
-# git submodule update --init --recursive
+# docker-machine start default && eval $(docker-machine env default)
 
-WORKDIR="`pwd`"
-SWIFT_PATH=$WORKDIR/build/buildbot_linux/none-swift_package_sandbox_linux-x86_64/usr/bin
-LINUX_SOURCEKIT_LIB_PATH=$WORKDIR/build/buildbot_linux/swift-linux-x86_64/lib
+BASE_IMAGE="swift-dev-15.10"
 
-DOCKER_IMAGE="swift-dev-15.10"
-DOCKER_RUN_OPTIONS="--privileged -it -v $WORKDIR:$WORKDIR -w $WORKDIR --rm"
+# swift build environments
+docker build -q -f Dockerfile-${BASE_IMAGE} -t ${BASE_IMAGE} .
 
-if [ ! -f "$SWIFT_PATH/swift" -o ! -f "$LINUX_SOURCEKIT_LIB_PATH/libsourcekitdInProc.so" ]; then
+WORK_DIR="`pwd`"
+DOCKER_RUN_OPTIONS="-it -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR} --rm"
+
+SOURCEKIT_IMAGE="sourcekit:30p2"
+REVISION="`git rev-parse --short HEAD|tr -d '\n'`"
+
+if [ -z "`docker images -q ${SOURCEKIT_IMAGE}|tr -d '\n'`" ]; then
+  SRC_DIR=${WORK_DIR}/swift
+  TOOLCHAIN_VERSION="swift-3.0-PREVIEW-2-${REVISION}-with-sourcekit"
+  ARCHIVE="${TOOLCHAIN_VERSION}.tar.gz"
+  SWIFT_INSTALLABLE_PACKAGE="${SRC_DIR}/${ARCHIVE}"
+  SWIFT_INSTALL_DIR="${SRC_DIR}/swift-nightly-install"
+
   # Build Swift With libdispatch
-
-  echo docker run $DOCKER_RUN_OPTIONS $DOCKER_IMAGE swift/utils/build-script --preset="buildbot_linux_libdispatch"
-  docker run $DOCKER_RUN_OPTIONS $DOCKER_IMAGE swift/utils/build-script --preset="buildbot_linux_libdispatch" || exit 1
+  docker run ${DOCKER_RUN_OPTIONS} ${BASE_IMAGE} \
+    swift/utils/build-script --preset="buildbot_linux_libdispatch" \
+    install_destdir="${SWIFT_INSTALL_DIR}" || exit 1
 
   # Build Normal Swift Toolchain & SourceKit
+  docker run ${DOCKER_RUN_OPTIONS} ${BASE_IMAGE} \
+    swift/utils/build-script --preset="buildbot_linux" \
+    install_destdir="${SWIFT_INSTALL_DIR}" \
+    installable_package="${SWIFT_INSTALLABLE_PACKAGE}" || exit 1
 
-  echo docker run $DOCKER_RUN_OPTIONS $DOCKER_IMAGE swift/utils/build-toolchain local.swift
-  docker run $DOCKER_RUN_OPTIONS $DOCKER_IMAGE swift/utils/build-toolchain local.swift || exit 1
+  DOCKER_BUILD_DIR="${TMPDIR}$(basename $0)"
+  rm -rf ${DOCKER_BUILD_DIR}
+  mkdir -p ${DOCKER_BUILD_DIR}
+  cp -p ${SWIFT_INSTALLABLE_PACKAGE} ${DOCKER_BUILD_DIR}/
+  cat <<-EOF >${DOCKER_BUILD_DIR}/Dockerfile
+  FROM ${BASE_IMAGE}
+  ADD ${ARCHIVE} /
+EOF
+  docker build -t ${SOURCEKIT_IMAGE} ${DOCKER_BUILD_DIR} || exit 1
 
 fi
 
 # Build SourceKitten
-docker run $DOCKER_RUN_OPTIONS \
-  -e "LINUX_SOURCEKIT_LIB_PATH=$LINUX_SOURCEKIT_LIB_PATH" \
-  -w $WORKDIR/SourceKitten \
-  $DOCKER_IMAGE /bin/sh -c "PATH=$SWIFT_PATH:\$PATH swift build" || exit 1
-
-docker run $DOCKER_RUN_OPTIONS \
-  -e "LINUX_SOURCEKIT_LIB_PATH=$LINUX_SOURCEKIT_LIB_PATH" \
-  -w $WORKDIR/SourceKitten \
-  $DOCKER_IMAGE /bin/sh -c "PATH=$SWIFT_PATH:\$PATH swift test" || exit 1
+docker run ${DOCKER_RUN_OPTIONS} \
+  -w ${WORK_DIR}/SourceKitten \
+  ${SOURCEKIT_IMAGE} /bin/sh -c "swift build && swift test" || exit 1
