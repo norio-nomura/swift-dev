9e2f6fd8cd47be955e44c09a8b3eed540d0e684c
Norio Nomura <norio.nomura@gmail.com>
2016-07-16 17:10:16 +0900

    Use Dockerfile-swift-15.10 in build-sourcekit-sv.sh

--- build-sourcekit-sv.sh
+++ build-sourcekit-sv.sh
@@ -4,18 +4,18 @@ cd "$(dirname $0)/." || exit
 
 # docker-machine start default && eval $(docker-machine env default)
 
-BASE_IMAGE="swift-dev-15.10"
+BUILD_BASE_IMAGE="swift-dev-15.10"
 
 # swift build environments
-docker build -q -f Dockerfile-${BASE_IMAGE} -t ${BASE_IMAGE} .
+docker build -q -f Dockerfile-${BUILD_BASE_IMAGE} -t ${BUILD_BASE_IMAGE} .
 
 WORK_DIR="`pwd`"
 DOCKER_RUN_OPTIONS="-it -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR} --rm"
 
 SOURCEKIT_IMAGE="sourcekit:sv"
-REVISION="`git rev-parse --short HEAD|tr -d '\n'`"
 
 if [ -z "`docker images -q ${SOURCEKIT_IMAGE}|tr -d '\n'`" ]; then
+  REVISION="`git rev-parse --short HEAD|tr -d '\n'`"
   SRC_DIR=${WORK_DIR}/swift
   TOOLCHAIN_VERSION="swift-3.0-PREVIEW-2-${REVISION}-with-sourcekit"
   ARCHIVE="${TOOLCHAIN_VERSION}.tar.gz"
@@ -23,16 +23,21 @@ if [ -z "`docker images -q ${SOURCEKIT_IMAGE}|tr -d '\n'`" ]; then
   SWIFT_INSTALL_DIR="${WORK_DIR}/build/swift-nightly-install"
 
   # Build Swift With libdispatch
-  docker run ${DOCKER_RUN_OPTIONS} ${BASE_IMAGE} \
+  docker run ${DOCKER_RUN_OPTIONS} ${BUILD_BASE_IMAGE} \
     swift/utils/build-script --preset="buildbot_linux_libdispatch" \
     install_destdir="${SWIFT_INSTALL_DIR}" || exit 1
 
   # Build Normal Swift Toolchain & SourceKit
-  docker run ${DOCKER_RUN_OPTIONS} ${BASE_IMAGE} \
+  docker run ${DOCKER_RUN_OPTIONS} ${BUILD_BASE_IMAGE} \
     swift/utils/build-script --preset="buildbot_linux" \
     install_destdir="${SWIFT_INSTALL_DIR}" \
     installable_package="${SWIFT_INSTALLABLE_PACKAGE}" || exit 1
 
+  # Build ${BASE_IMAGE}
+  BASE_IMAGE="swift-base-15.10"
+  docker build -f Dockerfile-swift-15 -t ${BASE_IMAGE} . || exit 1
+
+  # Build ${SOURCEKIT_IMAGE}
   DOCKER_BUILD_DIR="${TMPDIR}$(basename $0)"
   rm -rf ${DOCKER_BUILD_DIR}
   mkdir -p ${DOCKER_BUILD_DIR}
