e3209f96cd3d38232835dfe3042499a6f4b59e46
Norio Nomura <norio.nomura@gmail.com>
2016-07-29 10:34:48 +0900

    Use swift-DEVELOPMENT-SNAPSHOT-2016-07-28-a
    Using my forked repositories:
    - swift-corelibs-libdispatch
      - Disable failing tests
    - swiftpm
      - Add workaround for https://github.com/apple/swift-corelibs-libdispatch/pull/94

--- build-sourcekit-sv.sh
+++ build-sourcekit-sv.sh
@@ -17,7 +17,7 @@ SOURCEKIT_IMAGE="sourcekit:sv"
 if [ -z "`docker images -q ${SOURCEKIT_IMAGE}|tr -d '\n'`" ]; then
   REVISION="`git rev-parse --short HEAD|tr -d '\n'`"
   SRC_DIR=${WORK_DIR}/swift
-  TOOLCHAIN_VERSION="swift-DEVELOPMENT-SNAPSHOT-2016-07-25-a-${REVISION}-with-sourcekit"
+  TOOLCHAIN_VERSION="swift-DEVELOPMENT-SNAPSHOT-2016-07-28-a-${REVISION}-with-sourcekit"
   ARCHIVE="${TOOLCHAIN_VERSION}.tar.gz"
   SWIFT_INSTALLABLE_PACKAGE="${WORK_DIR}/build/${ARCHIVE}"
   SWIFT_INSTALL_DIR="${WORK_DIR}/build/swift-nightly-install"
